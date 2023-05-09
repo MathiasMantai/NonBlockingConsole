88d842b6e028176757ada9f299c7a00febf9e86f
MathiasMantai <mmantaibusiness@gmail.com>
2023-05-09 09:07:48 +0200

    first commit

---
+++ NonBlockingConsole.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Concurrent;
+
+namespace NFC_Recker_Eventbased.NonBlockingConsole
+{
+    /// <summary>
+    /// Class <c>NonBlockingConsole</c> creates a queue for asynchronous writing to console
+    /// implements a version of the producer consumer pattern
+    /// </summary>
+    static class NonBlockingConsole
+    {
+        private static BlockingCollection<string> queue = new BlockingCollection<string>();
+        
+        /// <summary>
+        /// Method <c>NonBlockingConsole</c> The constructor creates a separate thread, where strings are written to the console asynchronously
+        /// </summary>
+        static NonBlockingConsole() {
+            var thread = new Thread(() => {
+                  while(true)
+                  {
+                        Console.WriteLine(queue.Take());
+                  }
+            });
+            thread.IsBackground = true;
+            thread.Start();
+        } 
+        
+        /// <summary>
+        /// Method <c>WriteLine</c> adds strings to the queue. Will be used instead of the conventional WriteLine
+        /// </summary>
+        /// <param name="Message"></param>
+        public static void WriteLine(string Message)
+        {
+            queue.Add(Message); 
+        }
+    }
+}
