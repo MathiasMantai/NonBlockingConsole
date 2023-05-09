49ec926fe26451127a5a909b3a8dcbee3b12e4b8
MathiasMantai <mmantaibusiness@gmail.com>
2023-05-09 09:12:17 +0200

    chore: code documentation update

--- NonBlockingConsole.cs
+++ NonBlockingConsole.cs
@@ -3,7 +3,7 @@
 namespace NFC_Recker_Eventbased.NonBlockingConsole
 {
     /// <summary>
-    /// Class <c>NonBlockingConsole</c> creates a queue for asynchronous writing to console
+    /// Class <c>NonBlockingConsole</c> creates a queue for writing to console
     /// implements a version of the producer consumer pattern
     /// </summary>
     static class NonBlockingConsole
@@ -11,7 +11,7 @@ namespace NFC_Recker_Eventbased.NonBlockingConsole
         private static BlockingCollection<string> queue = new BlockingCollection<string>();
         
         /// <summary>
-        /// Method <c>NonBlockingConsole</c> The constructor creates a separate thread, where strings are written to the console asynchronously
+        /// Method <c>NonBlockingConsole</c> The constructor creates a separate thread, where strings are written to the console
         /// </summary>
         static NonBlockingConsole() {
             var thread = new Thread(() => {
@@ -27,7 +27,7 @@ namespace NFC_Recker_Eventbased.NonBlockingConsole
         /// <summary>
         /// Method <c>WriteLine</c> adds strings to the queue. Will be used instead of the conventional WriteLine
         /// </summary>
-        /// <param name="Message"></param>
+        /// <param name="Message">string to be added to the queue</param>
         public static void WriteLine(string Message)
         {
             queue.Add(Message); 
